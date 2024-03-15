d3f2d38c55a8a14d277556c36ce2ff38975cb47b
dzemal9017989 <114153831+dzemal9017989@users.noreply.github.com>
2024-03-15 14:30:40 +0100

    Add files via upload

---
+++ login_php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dzema/test',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e92f20dc3f391278c589c2e600f90b2e0f369dc4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dzema/test' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e92f20dc3f391278c589c2e600f90b2e0f369dc4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
